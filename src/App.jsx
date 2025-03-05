7fa10fab2592b9daa027de759041c145acff9808
Ratmir <ceratmir@gmail.com>
2025-03-05 19:52:14 +0600

    fff

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import React from 'react'
 
 function App() {
   return (
-    <Router basename="/TON">
+    <Router basename="/">
       {' '}
       {/* Добавляем basename */}
       <div className="container">
