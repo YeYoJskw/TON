b718fea2dd16f047c4c4308fb7ba2df1876a96fa
Ratmir <ceratmir@gmail.com>
2025-03-05 19:24:07 +0600

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/', // Указывает корневой путь для раздачи файлов
   plugins: [react()],
   build: {
     outDir: 'dist',
